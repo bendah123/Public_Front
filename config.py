2d5b1d493161b903740eec539a8f46d2cc18ace2
bendah123 <121314806+bendah123@users.noreply.github.com>
2023-12-11 16:43:14 +0200

    Update config.py

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ mqtt_transport = 'tcp' #tcp or websockets. Use websockets if issues with firewal
 mqtt_client_id ='ds-public-front'
 mqtt_username = 'ds-public-front'
 mqtt_password = ''
-url1 = http://svm-11-2.cs.helsinki.fi:8081
-url2 = http://svm-11.cs.helsinki.fi:8081
+url1 = 'http://svm-11-2.cs.helsinki.fi:8081/inventory'
+url2 = 'http://svm-11.cs.helsinki.fi:8081/order'
 
 mqtt_topic_on_order_send = 'public-front/order-send'
 mqtt_topic_on_order_canceled = 'public-front/order-canceled'
