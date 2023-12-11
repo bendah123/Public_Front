de7cb5f587c36a374267d06d5b14f675c27c57a1
bendah123 <121314806+bendah123@users.noreply.github.com>
2023-12-11 16:06:21 +0200

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ mqtt_client_id ='ds-public-front'
 mqtt_username = 'ds-public-front'
 mqtt_password = ''
 url1 = http://svm-11-2.cs.helsinki.fi:8081
-
+url2 = http://svm-11.cs.helsinki.fi:8081
 
 mqtt_topic_on_order_send = 'public-front/order-send'
 mqtt_topic_on_order_canceled = 'public-front/order-canceled'
