fbfda415bd6b7108d717422a33cbe026c31ac93b
Paananen Kalle E <kallepaa@svm-11.cs.helsinki.fi>
2023-12-14 19:27:29 +0200

    adjustments for demo

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ mqtt_port = 1883
 mqtt_transport = 'tcp' #tcp or websockets. Use websockets if issues with firewall
 mqtt_client_id ='ds-public-front'
 mqtt_username = 'ds-public-front'
-mqtt_password = 'E9SBLg:M2mhbG3w'
+mqtt_password = ''
 url1 = 'http://svm-11-2.cs.helsinki.fi:8081/inventory'
 url2 = 'http://svm-11.cs.helsinki.fi:8081/order'
 
